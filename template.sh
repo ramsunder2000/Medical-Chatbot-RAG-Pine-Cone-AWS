8eec7989ab060cd3c27635375e6b793c6b58973d
Ram Sunder <ramsunder2000@gmail.com>
2025-10-27 12:31:12 +0000

    Folder structure created

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Create Directory Structure for Project
+mkdir -p src
+mkdir -p research
+
+# Create File
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+echo "Project structure created successfully!!!."
